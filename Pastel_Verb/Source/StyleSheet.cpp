220e1ddd4fae6c1fe6345eea0f0a0aedb50534ac
Landon <33662627+landonviator@users.noreply.github.com>
2021-08-23 19:26:11 -0500

    Fixed plist warnings

--- Pastel_Verb/Source/StyleSheet.cpp
+++ Pastel_Verb/Source/StyleSheet.cpp
@@ -15,10 +15,7 @@ namespace juce
 void LV_CustomAbleDialLAF::drawRotarySlider (Graphics& g, int x, int y, int width, int height, float sliderPos, const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider)
 {
     auto outline = slider.findColour (Slider::rotarySliderOutlineColourId);
-    auto fill    = slider.findColour (Slider::rotarySliderFillColourId);
-
     auto bounds = Rectangle<int> (x, y, width, height).toFloat().reduced (10);
-
     auto radius = jmin (bounds.getWidth(), bounds.getHeight()) / 2.0f;
     auto toAngle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
     auto lineW = jmin (8.0f, radius * 0.5f);
